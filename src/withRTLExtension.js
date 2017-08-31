2711049db99678d4871e7ee010e3d970eee55557
Maja Wichrowska <maja.wichrowska@airbnb.com>
2017-08-31 15:39:24 -0700

    Fix matchmedia support in withRTLExtension and add tests

--- src/withRTLExtension.js
+++ src/withRTLExtension.js
@@ -1,5 +1,7 @@
 export const RTL_SELECTOR = '_rtl';
 
+const styleDefRegex = /\.[^{}]+\{[^{}]+\}/g;
+
 /*
  * When automatically flipping CSS styles in our interface, instead of determining RTL/LTR context
  * at the time of the create or resolve call (which is hard to do without either setting the
@@ -14,13 +16,9 @@ function directionSelectorHandler(selector, baseSelector, generateSubtreeStyles)
 
   const generated = generateSubtreeStyles(baseSelector);
 
-  // generated may include more than one style definition. We want to prefix each one individually.
-  // This primarily happens for pseudo selectors for which generated looks like
-  //   .classname::selector{...}.classname::otherselector{...}
-  // This is the only case we really worry about, and as a result, we split on '}.' which marks the
-  // end of style definition and beginning of a new one.
-  const styleDefs = generated.split('}.').map(g => `[dir="rtl"] ${g}`);
-  return styleDefs.join('}.');
+  // generated may include more than one style definition (pseudoselectors, matchmedia, etc.).
+  // We want to prefix each one individually.
+  return generated.replace(styleDefRegex, g => `[dir="rtl"] ${g}`);
 }
 
 export default function withRTLExtension({ StyleSheet } /* aphrodite */) {
