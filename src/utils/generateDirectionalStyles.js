bb8ab2dc4679748e445a279dd93a87837ca757ce
Maja Wichrowska <maja.wichrowska@airbnb.com>
2017-10-26 11:24:00 -0700

    Add fix for differing specificity between flippable and non-flippable versions of the same style

--- src/utils/generateDirectionalStyles.js
+++ src/utils/generateDirectionalStyles.js
@@ -2,11 +2,13 @@ import rtlCSSJS from 'rtl-css-js';
 
 import { LTR_SELECTOR, RTL_SELECTOR } from './withRTLExtension';
 
+const directionRegex = /Left|Right/;
+
 function isEmpty(obj) {
   return obj && Object.keys(obj).length === 0;
 }
 
-function separateDirectionalStyles(originalStyles, autoRTLStyles) {
+function separateDirectionalStyles(originalStyles, autoRTLStyles, directionalStyleKeys) {
   const sharedStyles = {};
   const ltrStyles = { ...originalStyles };
   const rtlStyles = {};
@@ -24,7 +26,8 @@ function separateDirectionalStyles(originalStyles, autoRTLStyles) {
         delete ltrStyles[key];
         // In some cases (pseudoselectors, matchmedia queries, etc.), the style
         // value may be an object, and we need to recurse.
-        const recursiveStyles = separateDirectionalStyles(originalStyles[key], value);
+        const recursiveStyles =
+          separateDirectionalStyles(originalStyles[key], value, directionalStyleKeys);
 
         if (recursiveStyles != null) {
           hasRTLStyles = true;
@@ -34,6 +37,7 @@ function separateDirectionalStyles(originalStyles, autoRTLStyles) {
             rtlStyles: recursiveRtlStyles,
           } = recursiveStyles;
 
+          directionalStyleKeys.add(key.replace(directionRegex, ''));
           if (recursiveSharedStyles) sharedStyles[key] = recursiveSharedStyles;
           if (recursiveLtrStyles) ltrStyles[key] = recursiveLtrStyles;
           if (recursiveRtlStyles) rtlStyles[key] = recursiveRtlStyles;
@@ -43,6 +47,7 @@ function separateDirectionalStyles(originalStyles, autoRTLStyles) {
       } else if (value != null) {
         hasRTLStyles = true;
         rtlStyles[key] = value;
+        directionalStyleKeys.add(key.replace(directionRegex, ''));
       }
     });
 
@@ -55,8 +60,12 @@ function separateDirectionalStyles(originalStyles, autoRTLStyles) {
   };
 }
 
-export default function generateDirectionalStyles(originalStyles) {
-  const directionalStyles = separateDirectionalStyles(originalStyles, rtlCSSJS(originalStyles));
+export default function generateDirectionalStyles(
+  originalStyles,
+  directionalStyleKeys = new Set(),
+) {
+  const directionalStyles =
+    separateDirectionalStyles(originalStyles, rtlCSSJS(originalStyles), directionalStyleKeys);
   if (!directionalStyles) return null;
 
   const { sharedStyles, ltrStyles, rtlStyles } = directionalStyles;
